b332c9ac042f9c798d1661e4a29cfc809d926843
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-28 14:22:46 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,6 +23,6 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  
-  #Hint:  __ + (( __ * __ ) / __ )
+  expect(order_of_operation(7, 43, 23, 83)).to eq(18)
+  num1 + ((num2 * num3)/num4)
 end
